ecdde749354afb6ebf1a8c13187392d60f1bd28d
fxrcode <fxrcode@gmail.com>
2020-11-30 03:32:10 -0800

    lc516 to start leetcode 0001

---
+++ README.md
@@ -0,0 +1,7 @@
+# LeetFlag
+
+## Total Count
+* (1/1715) Jan 9, 2021
+## Using vscode-leetcode
+* https://github.com/LeetCode-OpenSource/vscode-leetcode
+* logined via 3rd party but copy whole cookie from chrome inspect on /all header.
---
+++ app/src/test/java/leet/516.longest-palindromic-subsequence.java
@@ -0,0 +1,40 @@
+package leet;
+
+/*
+ * @lc app=leetcode id=516 lang=java
+ *
+ * [516] Longest Palindromic Subsequence
+ */
+
+// @lc code=start
+class Solution {
+    public int longestPalindromeSubseq(String ss) {
+        char[] s = ss.toCharArray();
+        int n = ss.length();
+        if (n <= 1)
+            return n;
+
+        int[][] f = new int[n][n];
+        int i,j,len;
+        for (i = 0; i < n; i++) {
+            f[i][i] = 1;
+        }
+        for (i = 0; i < n-1; ++i) {
+            f[i][i+1] = (s[i] == s[i+1]) ? 2 : 1;
+        }
+
+        for (len = 3; len <= n; len++){
+            for (i = 0; i <= n-len; i++) {
+                j = i + len -1;
+                // j < n => i < n-len+1
+                f[i][j] = Math.max(f[i+1][j], f[i][j-1]);
+                if (s[i] == s[j]) {
+                    f[i][j] = Math.max(f[i+1][j-1]+2, f[i][j]);
+                }
+            }
+        }
+        return f[0][n-1];
+    }
+}
+// @lc code=end
+
